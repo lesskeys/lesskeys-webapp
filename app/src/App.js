3f96fdaebf19c79647e347b9e1d86a0774ab1765
Lukas Dötlinger <lukas.doetlinger@student.uibk.ac.at>
2018-07-31 23:03:02 +0200

    Restructure app and add ring table.

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button, Table } from 'reactstrap'
 import logo from './logo.svg';
 import './App.css';
 
@@ -21,20 +22,33 @@ class App extends Component {
       return <p>Loading...</p>
     }
 
+    const userList = users.map(user => {
+      const email = user.email;
+      return <tr key={user.userId}>
+        <td style={{whiteSpace: 'nowrap'}}>{user.firstName}</td>
+        <td>
+          <Button size="sm" color="primary">Ring</Button>
+        </td>
+      </tr>
+    });
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <div className="Ring list">
-          <h2>Tenants</h2>
-          {users.map(user =>
-            <div key={user.userId}>
-              {user.email}
-            </div>
-          )}
-        </div>
+        <Table className="mt-4">
+          <thread>
+            <tr>
+              <th width="20%">Name</th>
+              <th width="10%">Actions</th>
+            </tr>
+          </thread>
+          <tbody>
+          {userList}
+          </tbody>
+        </Table>
       </div>
     );
   }
