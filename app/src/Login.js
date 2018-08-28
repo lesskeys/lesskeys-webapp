361d376d257ea4d07f079d383b6de61247f5e17c
Lukas Dötlinger <lukas.doetlinger@student.uibk.ac.at>
2018-08-28 16:50:45 +0200

    Design login page.

--- app/src/Login.js
+++ app/src/Login.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
+import './style/Login.css';
 
 class Login extends Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      code: ''
+    }
+  }
+
+  updateInputValue (evt) {
+    this.setState({
+      code: evt.target.value
+    })
+  }
 
   render () {
     return (
-      <div>
-        This is a test!
+      <div className="background">
+        <div className="center">
+          <div className="head">
+            Login
+          </div>
+          <form className="form">
+            <label>
+              Authentifizierungscode
+              <input type="text" placeholder="Code" value={this.state.code} onChange={(e) => this.updateInputValue(e)} />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
       </div>
     );
   }
